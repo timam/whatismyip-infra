eba15e0fb4bbd0ebcd225f44ef133c5f21b54ed5
Al-Amin Timam <mail@timam.me>
2021-08-09 17:42:13 +0600

    Added whatismyip app

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -3,9 +3,22 @@ module "api" {
   env       = local.env
   namespace = local.project
 
-  microservice_name = "${local.project}-api"
+  microservice_name = "${local.env}-${local.project}-api"
   health_check_path = "/"
   docker_image      = "timam/${local.project}-api"
+  docker_image_tag  = "v1"
+  container_port    = "8080"
+  service_port      = "8080"
+}
+
+module "app" {
+  source    = "../tf-modules/deployment"
+  env       = local.env
+  namespace = local.project
+
+  microservice_name = "${local.env}-${local.project}-app"
+  health_check_path = "/"
+  docker_image      = "timam/${local.project}-app"
   docker_image_tag  = "v1"
   container_port    = "8080"
   service_port      = "8080"
